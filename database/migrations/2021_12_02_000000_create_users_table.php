b1ce8c512aa333572e04dd9faabdd369b1fc1c86
oakley.chen <oakley.chen@smartclouds.info>
2021-12-13 11:55:41 +0800

    feature: 新增使用者資料migration
    star-users-table-migration

--- database/migrations/2021_12_02_000000_create_users_table.php
+++ database/migrations/2021_12_02_000000_create_users_table.php
@@ -15,12 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('account')->unique();
+            $table->string('nickname');
+            $table->bigInteger('group_id');
             $table->string('password');
-            $table->rememberToken();
+            $table->tinyInteger('enable');
             $table->timestamps();
+            $table->timestamp('last_login_at')->nullable();
         });
     }
 
